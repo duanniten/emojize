490fe82e995d54a8b826cc6db5505e324c0ed144
duanniten <duann_@hotmail.com>
2024-09-22 10:39:35 -0300

    Implemeetationof emojize

---
+++ emojize.py
@@ -0,0 +1,27 @@
+from emoji import emojize
+import sys
+
+def main():
+    #read emotion code
+    emoti = read_emotion()
+
+    #get emotion from emojize
+    emoti = get_emotion(emoti)
+
+    #print emoji
+    print_emotion(emoti)
+
+def print_emotion(emoti: str):
+    print(f'Output: {emoti}')
+
+def read_emotion():
+    return input("Input: ").strip()
+
+def get_emotion(emoti):
+    try:
+        emoti = emojize(emoti, delimiters= [':', ':'])
+    except:
+        sys.exit("Emoji not found")
+
+if __name__ == '__main__':
+    main()
